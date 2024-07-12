312a26d19834731f8e8b66a9af154d20915f391f
Thomas Couloud <thomascouloud@hotmail.fr>
2024-07-12 16:00:46 +0200

    black & pip

--- DowJones project/tests/test_api.py
+++ DowJones project/tests/test_api.py
@@ -1,6 +1,6 @@
-
 import sys
 import os
+
 path_to_projectroot = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(path_to_projectroot)
 from fastapi.testclient import TestClient
@@ -14,6 +14,7 @@ client = TestClient(app)
 TICKERS = ["AAPL", "MSFT", "AXP"]
 
 for ticker in TICKERS:
+
     def test_predict_endpoint_for_ticker():
         response = client.post("/api/predict/", json={"ticker": ticker, "days": 5})
         assert response.status_code == 200
@@ -21,7 +22,11 @@ for ticker in TICKERS:
         assert "predictions" in data
         assert "mse" in data
 
+
 def test_predict_endpoint_days_exceed():
     response = client.post("/api/predict/", json={"ticker": "AAPL", "days": 9999})
     assert response.status_code == 400
-    assert response.json()["detail"] == "Le nombre de jours demandés dépassent les données de test disponibles"
+    assert (
+        response.json()["detail"]
+        == "Le nombre de jours demandés dépassent les données de test disponibles"
+    )
--- DowJones project/tests/test_models.py
+++ DowJones project/tests/test_models.py
@@ -1,9 +1,10 @@
-
 import sys
-#sys.path.append("Project-root/api")
+
+# sys.path.append("Project-root/api")
 ## changed the path to project root in a relative not to get broken post commit / merge
 import os
-path_to_api = os.path.join(os.path.dirname(os.path.dirname(__file__)),'api')
+
+path_to_api = os.path.join(os.path.dirname(os.path.dirname(__file__)), "api")
 sys.path.append(path_to_api)
 import pytest
 import numpy as np
@@ -14,25 +15,20 @@ from api.model_training import train_and_save_model
 def test_xgboost_model_training_and_prediction():
     ticker = "AAPL"
     data = load_data(ticker)
-    
-  
+
     train_X, train_y, _, _, _ = preprocess_data(data)
-    
-   
-    best_params = { 
-        'nthread': [4], 
-        'objective': ['reg:squarederror'], 
-        'learning_rate': [0.03, 0.05, 0.07], 
-        'max_depth': [5, 6, 7], 
-        'min_child_weight': [4], 
-        'silent': [1], 
-        'subsample': [0.7], 
-        'colsample_bytree': [0.7], 
-        'n_estimators': [100, 500, 1000]
+
+    best_params = {
+        "nthread": [4],
+        "objective": ["reg:squarederror"],
+        "learning_rate": [0.03, 0.05, 0.07],
+        "max_depth": [5, 6, 7],
+        "min_child_weight": [4],
+        "silent": [1],
+        "subsample": [0.7],
+        "colsample_bytree": [0.7],
+        "n_estimators": [100, 500, 1000],
     }
 
-   
     result = train_and_save_model(train_X, train_y, best_params)
     assert "best_params" in result
-
-
